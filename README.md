42e9886e01938ccb43a860210bcf8e4f678ab9a2
Tomasz Pielczyk <tomasz.pielczyk@gmail.com>
2023-12-23 13:55:20 +0100

    init

---
+++ README.md
@@ -0,0 +1,63 @@
+# flux
+
+Monitor long running script
+
+## Requirments
+
+- PHP >= 8
+
+## Installation
+
+### Using skeleton
+
+[xtompie/monitor](https://github.com/xtompie/monitor)
+
+## Doc
+
+### Basic exmaple
+
+```php
+<?php
+
+use Xtompie\Monitor\Monitor;
+
+$monitor = new Monitor(name: 'import', stdout: true, frequency: 1);
+foreach (input() as $input) {
+    $imported = import($input);
+    $imported ? $monitor->up('done') : $monitor->up('skip');
+}
+$monitor->show();
+```
+
+It will generator output in stdout:
+
+```plain
+#import | 2023-12-23 12:06:06 | 2023-12-23 12:06:10 | 0:00:00:04 | 2 mb | done: 2
+#import | 2023-12-23 12:06:06 | 2023-12-23 12:06:12 | 0:00:00:06 | 2 mb | done: 5
+#import | 2023-12-23 12:06:06 | 2023-12-23 12:06:12 | 0:00:00:06 | 2 mb | done: 5 | skip: 3
+```
+
+A new line with the monitor status is generated not Czech than `frequency` when the monitor status changes.
+
+For more check source code: [Monitor](https://github.com/xtompie/monitor/blob/master/src/Monitor.php)
+
+### Creating specific monitor
+
+```php
+<?php
+
+use Xtompie\Monitor\Monitor;
+
+class ImportMonitor extends Monitor
+{
+    public function done(): void
+    {
+        $this->up('done');
+    }
+
+    public function skip(): void
+    {
+        $this->up('skip');
+    }
+}
+```
---
+++ src/Monitor.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xtompie\Monitor;
+
+class Monitor
+{
+    public function __construct(
+        protected string $name = 'monitor',
+        protected bool $stdout = false,
+        protected int $frequency = 1,
+        protected array $data = [],
+        protected ?int $start = null,
+        protected ?int $last = null,
+    ) {
+        $this->start = time();
+    }
+
+    public function up(string $property, int $value = 1): void
+    {
+        if (!isset($this->data[$property])) {
+            $this->data[$property] = 0;
+        }
+        $this->data[$property] = $this->data[$property] + $value;
+        $this->update();
+    }
+
+    public function down(string $property, int $value = 1): void
+    {
+        $this->data[$property] = $this->data[$property] - $value;
+        $this->update();
+    }
+
+    public function set(string $property, mixed $value): void
+    {
+        $this->data[$property] = $value;
+        $this->update();
+    }
+
+    public function stdout(bool $stdout): static
+    {
+        $this->stdout = $stdout;
+        return $this;
+    }
+
+    public function show(): void
+    {
+        if ($this->stdout === false) {
+            return;
+        }
+
+        echo $this->render();
+    }
+
+    protected function update(): void
+    {
+        if ($this->stdout === false) {
+            return;
+        }
+
+        if ($this->last === null || time() - $this->last > $this->frequency) {
+            $this->last = time();
+            echo $this->render();
+        }
+    }
+
+    public function render(): string
+    {
+        return '#' . $this->name
+            . ' | ' . date('Y-m-d H:i:s', $this->start)
+            . ' | ' . date('Y-m-d H:i:s')
+            . ' | ' . $this->secondsHr(time() - $this->start)
+            . ' | ' . $this->sizeHr(memory_get_usage(true))
+            . ' | ' . $this->state()
+            . "\n"
+        ;
+    }
+
+    protected function state(): string
+    {
+        return implode(' | ', array_map(fn ($k, $v) => "$k: $v", array_keys($this->data), $this->data));
+    }
+
+    protected function sizeHr(int $size): string
+    {
+        $unit = ['b', 'kb', 'mb', 'gb', 'tb', 'pb'];
+        return @round($size / pow(1024, ($i = floor(log($size, 1024)))), 2) . ' ' . $unit[$i];
+    }
+
+    protected function secondsHr(int $seconds): string
+    {
+        $s = intval($seconds);
+        return @sprintf('%d:%02d:%02d:%02d', intval($s / 86400), intval($s / 3600) % 24, intval($s / 60) % 60, intval($s % 60));
+    }
+}
